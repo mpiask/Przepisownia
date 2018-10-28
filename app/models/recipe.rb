7fbed1d74556e0044a666c31e80fdfe6a3fc751b
tojawawel <bgawel93@gmail.com>
2018-10-28 17:44:54 +0100

    Add validations to new recipe and new category

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipes_categories
   has_many :categories, through: :recipes_categories
+  validates :title, presence: true, length: { minimum: 3, maximum: 50}
+  validates :description, presence: true
+  validates :user_id, presence: true
 end
